518d929dcb7d7e967226a662bf7c7ea817770c3a
Martin Špilák <martinecko24@gmail.com>
2020-05-31 22:04:56 +0200

    Color Change

--- What-Should-I-Do/What-Should-I-Do/ReminderColor.cs
+++ What-Should-I-Do/What-Should-I-Do/ReminderColor.cs
@@ -5,7 +5,7 @@ namespace What_Should_I_Do
 {
     public class ReminderColor 
     {
-        public static Color[] colors = { Color.LightBlue, Color.LightGreen, Color.LightYellow, Color.LightPink, Color.LightSalmon, Color.LightSkyBlue, Color.Blue, Color.Green, Color.Yellow, Color.Pink, Color.Red };
+        public static Color[] colors = { Color.Black};
         public static Color RandomColor => GetRandomColor();
 
         public static Color GetRandomColor()
